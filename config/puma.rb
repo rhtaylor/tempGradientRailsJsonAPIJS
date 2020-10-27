998dc7968250883426beff27953e26e08fae7f15
Randy <randyisbusy@gmail.com>
2020-10-27 12:28:29 -0700

    updates database.yml

--- config/puma.rb
+++ config/puma.rb
@@ -7,8 +7,6 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
-require 'erb'
-ActiveRecord::Base.establish_connection( YAML.load( ERB.new( File.read( "#{app_dir}/config/database.yml" )).result)[rails_env])
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
